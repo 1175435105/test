6be782ea31440448507c70c80274be44689dd349
1175435105 <65878137+1175435105@users.noreply.github.com>
2021-02-22 10:16:15 +0800

    added a file

---
+++ temp.py
@@ -0,0 +1,9 @@
+import numpy as np
+from math import pi,e,sqrt,sin,cos,asin,acos,log
+
+x=np.linspace(0,2*pi,256)
+	
+for i in range(25):
+	print("4096,4096,4096,4096,4096,4096,4096,4096,4096,4096,")
+
+
